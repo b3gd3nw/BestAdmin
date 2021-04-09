bdd751027699c8a7d6f82d30831764a110cfa82f
kubitckiy_bogdan <bk@albedo.dev>
2021-04-09 18:36:35 +0300

    add employes table

--- resources/views/Admin/general/home.blade.php
+++ resources/views/Admin/general/home.blade.php
@@ -41,5 +41,45 @@
                 </div>
             </div>
         </div>
+        <div class="columns">
+            <div class="column is-full">
+                <h1 class="title">Employes</h1>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Email</th>
+                        <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tbody id="tbody">
+                    @foreach($employes as $employee)
+                        <tr>
+                            <th>{{ $employee['id'] }}</th>
+                            <td>{{ $employee['firstname'] . ' ' . $employee['lastname'] }}</td>
+                            <td>{{ $employee['position'] }}</td>
+                            <td><a href="mailto:">{{ $employee['email'] }}</a></td>
+                            @switch($employee['status'])
 
+                                @case('active')
+                                <td class="green">{{ $employee['status'] }}</td>
+                                @break
+
+                                @case('pending')
+                                <td class="orange">{{ $employee['status'] }}</td>
+                                @break
+
+                                @case('inactive')
+                                <td class="red">{{ $employee['status'] }}</td>
+                                @break
+
+                            @endswitch
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
 @endsection
