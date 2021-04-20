f14d170f1586a1c8e0d36a27d9da333ee6e62feb
kubitckiy_bogdan <bk@albedo.dev>
2021-04-20 15:35:34 +0300

    add skills into employee tabless

--- resources/views/Admin/general/home.blade.php
+++ resources/views/Admin/general/home.blade.php
@@ -51,6 +51,7 @@
                         <th>Name</th>
                         <th>Position</th>
                         <th>Email</th>
+                        <th>Skills</th>
                         <th>Status</th>
                     </tr>
                     </thead>
@@ -61,6 +62,17 @@
                             <td>{{ $employee['firstname'] . ' ' . $employee['lastname'] }}</td>
                             <td>{{ $employee['position'] }}</td>
                             <td><a href="mailto:">{{ $employee['email'] }}</a></td>
+                            <td>
+                                @foreach($employee_skills as $employee_skill)
+                                    @if($employee->id === $employee_skill->employeeId)
+                                        @foreach($skills as $skill)
+                                            @if($employee_skill->skillId === $skill->id)
+                                                <span class="tag">{{ $skill->name }}</span>
+                                            @endif
+                                        @endforeach
+                                    @endif
+                                @endforeach
+                            </td>
                             @switch($employee['status'])
 
                                 @case('active')
