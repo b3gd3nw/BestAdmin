f23322af0bedef3e2d788466db633c0f62e46dd8
kubitckiy_bogdan <bk@albedo.dev>
2021-04-26 16:51:56 +0300

    add labels and remove placeholders

--- resources/views/modals/editemployee.blade.php
+++ resources/views/modals/editemployee.blade.php
@@ -3,24 +3,28 @@
     @method('PUT')
     <div class="field">
         <p class="control">
-            <input name="firstname" class="input" type="text" placeholder="First Name" value="{{ $employee['firstname'] }}" require notnum max20>
+            <label for="firstname">First Name</label>
+            <input name="firstname" class="input" type="text" value="{{ $employee['firstname'] }}" require notnum max20>
             <span class="error red fs-12"></span>
         </p>
     </div>
     <div class="field">
         <p class="control">
-            <input name="lastname" class="input" type="text" placeholder="Last Name" value="{{ $employee['lastname'] }}" require notnum max20>
+            <label for="lastname">Last Name</label>
+            <input name="lastname" class="input" type="text" value="{{ $employee['lastname'] }}" require notnum max20>
             <span class="error red fs-12"></span>
         </p>
     </div>
     <div class="field">
         <p class="control">
-            <input name="birthdate" class="input" type="date" placeholder="Birthdate" max="{{ $today }}" value="{{ $employee['birthdate'] }}" onkeydown="return false" require>
+            <label for="birthdate">Birth Date</label>
+            <input name="birthdate" class="input" type="date" max="{{ $today }}" value="{{ $employee['birthdate'] }}" onkeydown="return false" require>
             <span class="error red fs-12"></span>
         </p>
     </div>
     <div class="field">
         <div class="control">
+            <label for="countryId">Country</label>
             <select name="countryId" class="input" id="" require>
                 @foreach($countries as $country)
                     <option value="{{ $country['id'] }}"
@@ -36,36 +40,42 @@
     </div>
     <div class="field">
         <p class="control">
-            <input name="position" class="input" type="text" placeholder="Position" value="{{ $employee['position'] }}" require>
+            <label for="position">Position</label>
+            <input name="position" class="input" type="text" value="{{ $employee['position'] }}" require>
             <span class="error red fs-12"></span>
         </p>
     </div>
     <div class="field">
         <p class="control">
-            <input name="email" class="input" type="email" placeholder="Email" value="{{ $employee['email'] }}" require email>
+            <label for="email">Email</label>
+            <input name="email" class="input" type="email" value="{{ $employee['email'] }}" require email>
             <span class="error red fs-12"></span>
         </p>
     </div>
     <div class="field">
         <p class="control">
-            <input name="phone" class="input" type="text" placeholder="Phone" id="phone" value="{{ $employee['phone'] }}" require min16>
+            <label for="phone">Phone</label>
+            <input name="phone" class="input" type="text" id="phone" value="{{ $employee['phone'] }}" require min16>
             <span class="error red fs-12"></span>
         </p>
     </div>
     <div class="field">
         <p class="control">
-            <input name="salary" class="input" type="text" placeholder="Salary" id="salary" value="{{ $employee['salary'] }}" money max6>
+            <label for="salary">Salary</label>
+            <input name="salary" class="input" type="text" id="salary" value="{{ $employee['salary'] }}" money max6>
             <span class="error red fs-12"></span>
         </p>
     </div>
     <div class="field">
         <p class="control">
-            <input name="skills" id="tags" class="input" type="tags" placeholder="Your skills" value="@foreach($skills as $skill){{ $skill . ',' }} @endforeach" reqtag taglength nodup>
+            <label for="skills">Skills</label>
+            <input name="skills" id="tags" class="input" type="tags" value="@foreach($skills as $skill){{ $skill . ',' }} @endforeach" reqtag taglength nodup>
             <span class="error red fs-12"></span>
         </p>
     </div>
     <div class="field">
         <p class="control">
+            <label for="status">Status</label>
             <select name="status" class="input" id="" require>
                 @switch($employee['status'])
                     @case('active')
